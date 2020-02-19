20bdd0cfb4683dee90d827bc61f0e66b43265d49
ykhade <ykhade@nevada.unr.edu>
2020-02-18 18:24:42 -0800

    inital commit

---
+++ flask-app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+https://media.giphy.com/media/Ma0mkiZtkdcLcJjsp0/giphy.gif
+images = [
+    "https://media.giphy.com/media/Ma0mkiZtkdcLcJjsp0/giphy.gif",
+
+]
+
+
+@app.route("/")
+def index():
+    url = random.choice(images)
+    return render_template("index.html", url=url)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
